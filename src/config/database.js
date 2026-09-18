fc2f1d6c446ebce75e51ceaa1b1d94cc52bc6f9e
ALUNO SESI PETROLINA <eduardolimar245@gmail.com>
2026-09-18 17:23:26 -0300

    codigo atualizado

--- src/config/database.js
+++ src/config/database.js
@@ -5,6 +5,13 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+   
+   ssl: {
+        rejectUnauthorized: true,
+    },
+   
+   
+   
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
